2a6dd744f09e7cecb8ff5c760debd330dd3fc2f1
DevIItalo <italoppttuu@gmail.com>
2023-03-10 14:57:44 -0300

    Commit
    Iniciando no JavaScript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btnProximo = document.querySelector("#proximo");
+const btnVolta = document.querySelector("#volta");
+
+btnVolta.addEventListener("click", ()=> {
+    alert('Você voltou');
+});
+
+btnProximo.addEventListener("click", ()=> {
+    alert('Proxima foto');
+});
